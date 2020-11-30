a77369df992abf6fc40e499d01e9c4d3a07585e1
rael5000 <barreto.israel5000@gmail.com>
2020-11-30 11:59:24 -0300

    First Commit

---
+++ Data/MvcAAPCContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AAPC.Models;
+
+namespace AAPC.Data
+{
+    public class MvcAAPCContext : DbContext
+    {
+        public MvcAAPCContext(DbContextOptions<MvcAAPCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Participante> Participante { get; set; }
+
+        public DbSet<Treino> Treino { get; set; }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,102 @@
+﻿using AAPC.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAPC.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcAAPCContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcAAPCContext>>()))
+            {
+                // Look for any movies.
+                if (context.Participante.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Participante.AddRange(
+                    new Participante
+                    {
+                        Nome = "Participante1",
+                        Sobrenome = "Sobrenome1",
+                        Nascimento = DateTime.Parse("1989-2-12"),
+                        DiaDoTreino = "Terca-Feira"
+                        //Genre = "Romantic Comedy",
+                        //Price = 7.99M
+                    },
+
+                    new Participante
+                    {
+                        Nome = "Participante2",
+                        Sobrenome = "Sobrenome2",
+                        Nascimento = DateTime.Parse("1989-2-12"),
+                        DiaDoTreino = "Quinta-Feira"
+                        //Genre = "Romantic Comedy",
+                        //Price = 7.99M
+                    },
+
+                    new Participante
+                    {
+                        Nome = "Participante3",
+                        Sobrenome = "Sobrenome3",
+                        Nascimento = DateTime.Parse("1989-2-12"),
+                        DiaDoTreino = "Sabado"
+                        //Genre = "Romantic Comedy",
+                        //Price = 7.99M
+                    },
+
+                    new Participante
+                    {
+                        Nome = "Participante4",
+                        Sobrenome = "Sobrenome4",
+                        Nascimento = DateTime.Parse("1989-2-12"),
+                        DiaDoTreino = "Terca-Feira"
+                        //Genre = "Romantic Comedy",
+                        //Price = 7.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+
+            using (var context = new MvcAAPCContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcAAPCContext>>()))
+            {
+                // Look for any movies.
+                if (context.Treino.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Treino.AddRange(
+                    new Treino
+                    {
+                        Dia = "Terça-Feira",
+                        Horario = "20h",
+                        Local = "Imbui"
+                        //Genre = "Romantic Comedy",
+                        //Price = 7.99M
+                    },
+
+                    new Treino
+                    {
+                        Dia = "Quinta-Feira",
+                        Horario = "20h",
+                        Local = "Imbui"
+                        //Genre = "Romantic Comedy",
+                        //Price = 7.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
